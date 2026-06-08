0c9025bf981b20110988eed2598fe1e7dd2398b8
jhervoch <marvin@42fr>
2026-06-08 16:15:06 +0200

    refactor: enhance docstrings for clarity in QLearningAgent methods

--- ai/Qlearning_agent.py
+++ ai/Qlearning_agent.py
@@ -12,12 +12,17 @@ class QLearningAgent:
         self.actions = ["UP", "DOWN", "LEFT", "RIGHT"]
 
     def get_q_values(self, state):
-        # mutable variable return a ref
+        """Returns the Q-values for the given state,
+        initializing them to 0 if the state is not in the Q-table.
+
+        It is a mutable variable, so it returns a reference
+        to the Q-values dictionary for the state."""
         if state not in self.q_table:
             self.q_table[state] = {action: 0.0 for action in self.actions}
         return self.q_table[state]
 
     def choose_action(self, state):
+        """Chooses an action based on the ε-greedy policy."""
         # exploration_prob (ε): Probability of taking a random action.
         if random.random() < self.epsilon:
             return random.choice(self.actions)
@@ -25,7 +30,10 @@ class QLearningAgent:
         return max(q_values, key=q_values.get)
 
     def learn(self, state, action, reward, next_state, done):
-        # Bellman equatiom
+        """Updates the Q-table based on
+        the action taken and the reward received.
+        """
+        # Bellman equation
         # Q[s, a] := Q[s, a] + α[r + γ maxa' Q(s', a') - Q(s, a)]
         # Q(s, a) is the Q-value for a given state-action pair.
         # R(s, a) is the immediate reward for taking action a in state s.
@@ -44,6 +52,7 @@ class QLearningAgent:
         q_values[action] = current_q + self.alpha * (target - current_q)
 
     def decay_epsilon(self):
+        """Decays the exploration rate ε after each episode."""
         return max(
             self.epsilon_min,
             self.epsilon * self.epsilon_decay
