2762a2af16d7497fd44bc234bf51b153c4fa0b49
jhervoch <marvin@42fr>
2026-06-29 09:30:32 +0200

    fix: update device selection to use CUDA instead of MPS for better compatibility

--- ai/DeepQLearning_agent.py
+++ ai/DeepQLearning_agent.py
@@ -17,7 +17,7 @@ HIDDEN_SIZE_2 = 32
 OUTPUT_SIZE = 4
 
 
-DEVICE = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class DeepQAgent:
